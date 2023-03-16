053f55739923617f8c7faf17621bcab2af854454
Sejung <kims762@newschool.edu>
2023-03-16 09:53:06 -0400

    Create JavaScript Solution 1 for question Two Number Sum

---
+++ Two Number Sum/Solution 1/Two Number Sum.js
@@ -0,0 +1,14 @@
+function twoNumberSum(array, targetSum) {
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      let sum = array[i] + array[j];
+      if (sum === targetSum) return [array[i], array[j]];
+    }
+  }
+  return [];
+}
+
+
+// Do not edit the line below.
+exports.twoNumberSum = twoNumberSum;
+
