e9c6eb10e2aa98c5c70b642a0427ff3e5fee327d
Sejung <kims762@newschool.edu>
2023-03-18 07:57:59 -0400

    Change JavaScript Solution 1 for question Subarray Sort

--- Subarray Sort/Solution 1/Subarray Sort.js
+++ Subarray Sort/Solution 1/Subarray Sort.js
@@ -1,9 +1,6 @@
 function subarraySort(array) {
   let first = Infinity;
   let last = -Infinity;
-  // How do we know a subarray is out of order?
-  // larger number comes after
-  // smaller number comes before
   for (let i = 0; i < array.length-1; i++) {
     let currNum = array[i];
     for (let j = i + 1; j < array.length; j++) {
@@ -11,7 +8,7 @@ function subarraySort(array) {
 
 
       if (traverse < currNum) {
-        if (traverse === currNum && j > i+ 1 || traverse < currNum) {
+        if (j > i+ 1 || traverse < currNum) {
           first = Math.min(first, i);
           last = Math.max(last, j);
         } 
