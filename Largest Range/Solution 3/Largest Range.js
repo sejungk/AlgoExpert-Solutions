ffa56ae17f3f829125dc8205abd1026859993a6a
Sejung <kims762@newschool.edu>
2023-03-20 09:10:00 -0400

    Change JavaScript Solution 3 for question Largest Range

---
+++ Largest Range/Solution 3/Largest Range.js
@@ -0,0 +1,48 @@
+function largestRange(array) {
+  let longestRange = [];
+  let range = 0;
+  
+  for (const num of array) {
+    if (!nums[num]) nums[num] = true;
+  }
+
+
+  for (const num of array) {
+    let startingIdx = num;
+    let endingIdx = num;
+    
+    if (nums[num] === true) {
+      let currRange = 1;
+      let left = num;
+      let right = num;
+
+
+      nums[num] = false;
+      
+      while (nums[left - 1]) {
+        nums[left] = false;
+        currRange++;
+        startingIdx = left;
+        left -= 1;
+      }
+      while (nums[right + 1]) {
+        nums[right] = false;
+        currRange++;
+        endingIdx = right;
+        right += 1;
+      }
+
+
+      if (currRange > range) {
+        range = currRange;
+        longestRange = [left, right]
+      }
+    }
+  }
+  return longestRange;
+}
+
+
+// Do not edit the line below.
+exports.largestRange = largestRange;
+
