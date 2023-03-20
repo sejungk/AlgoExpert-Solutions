0c72bed986d47e658fe5ee4c1f76d96d663fae03
Sejung <kims762@newschool.edu>
2023-03-20 11:51:55 -0400

    Change JavaScript Solution 3 for question Min Rewards

--- Min Rewards/Solution 3/Min Rewards.js
+++ Min Rewards/Solution 3/Min Rewards.js
@@ -1,5 +1,6 @@
 function minRewards(scores) {
-  const rewards = scores.map(_ => 1);
+  const rewards = new Array(scores.length).fill(1);
+  console.log(rewards)
   for (let i = 1; i < scores.length; i++) {
     if (scores[i] > scores[i - 1]) rewards[i] = rewards[i - 1] + 1;
   }
