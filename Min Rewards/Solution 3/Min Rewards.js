b6e1775be1de7053786b172979c0f9b45d438c23
Sejung <kims762@newschool.edu>
2023-03-20 11:47:59 -0400

    Create JavaScript Solution 3 for question Min Rewards

---
+++ Min Rewards/Solution 3/Min Rewards.js
@@ -0,0 +1,15 @@
+function minRewards(scores) {
+  const rewards = scores.map(_ => 1);
+  for (let i = 1; i < scores.length; i++) {
+    if (scores[i] > scores[i - 1]) rewards[i] = rewards[i - 1] + 1;
+  }
+  for (let i = scores.length - 2; i >= 0; i--) {
+    if (scores[i] > scores[i + 1]) rewards[i] = Math.max(rewards[i], rewards[i +1] + 1)
+  }
+  return rewards.reduce((a, b) => a + b);
+}
+
+
+// Do not edit the line below.
+exports.minRewards = minRewards;
+
