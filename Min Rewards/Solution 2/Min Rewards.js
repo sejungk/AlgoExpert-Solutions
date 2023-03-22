9da4caff9305ffcbe0f47e182a6a26b09da685e9
Sejung <kims762@newschool.edu>
2023-03-22 10:41:23 -0400

    Change JavaScript Solution 2 for question Min Rewards

---
+++ Min Rewards/Solution 2/Min Rewards.js
@@ -0,0 +1,30 @@
+function minRewards(scores) {
+  let rewards = new Array(scores.length).fill(1);
+
+
+  for (let i = 1; i < scores.length; i++) {
+    let prev = scores[i-1];
+    let curr = scores[i];
+
+
+    if (prev < curr) rewards[i] = rewards[i-1] + 1
+  }
+
+
+  for (let i = scores.length-1; i >= 0; i--) {
+    let prev = scores[i+1];
+    let curr = scores[i];
+
+
+    if (prev < curr) rewards[i] = Math.max(rewards[i+1] + 1, rewards[i])
+  }
+  let sum = rewards.reduce(function(a, b){
+    return a + b;
+  });
+  return sum;
+}
+
+
+// Do not edit the line below.
+exports.minRewards = minRewards;
+
