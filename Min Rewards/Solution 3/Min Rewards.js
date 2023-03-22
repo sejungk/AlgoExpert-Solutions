3c0ba4244d9c035fa35a02379d78543ff3daebf0
Sejung <kims762@newschool.edu>
2023-03-22 10:58:50 -0400

    Change JavaScript Solution 3 for question Min Rewards

--- Min Rewards/Solution 3/Min Rewards.js
+++ Min Rewards/Solution 3/Min Rewards.js
@@ -1,16 +1,37 @@
 function minRewards(scores) {
-  const rewards = new Array(scores.length).fill(1);
-  console.log(rewards)
-  for (let i = 1; i < scores.length; i++) {
-    if (scores[i] > scores[i - 1]) rewards[i] = rewards[i - 1] + 1;
-  }
-  for (let i = scores.length - 2; i >= 0; i--) {
-    if (scores[i] > scores[i + 1]) rewards[i] = Math.max(rewards[i], rewards[i +1] + 1)
+ const rewards = new Array(scores.length).fill(1);
+  const localMinIdxs = getLocalMinIdxs(scores);
+  for (const localMinIdx of localMinIdxs) {
+    expandFromLocalMinIdx(localMinIdx, scores, rewards);
   }
   return rewards.reduce((a, b) => a + b);
 }
 
 
-// Do not edit the line below.
+function getLocalMinIdxs(array) {
+  if (array.length === 1) return [0];
+  const localMinIdxs = [];
+  for (let i = 0; i < array.length; i++) {
+    if (i === 0 && array[i] < array[i + 1]) localMinIdxs.push(i);
+    if (i === array.length - 1 && array[i] < array[i - 1]) localMinIdxs.push(i);
+    if (i === 0 || i === array.length - 1) continue;
+    if (array[i] < array[i + 1] && array[i] < array[i - 1]) localMinIdxs.push(i);
+  }
+  return localMinIdxs;
+}
+
+
+function expandFromLocalMinIdx(localMinIdx, scores, rewards) {
+  let leftIdx = localMinIdx - 1;
+  while (leftIdx >= 0 && scores[leftIdx] > scores[leftIdx + 1]) {
+    rewards[leftIdx] = Math.max(rewards[leftIdx], rewards[leftIdx + 1] + 1);
+    leftIdx--;
+  }
+  let rightIdx = localMinIdx + 1
+  while (rightIdx < scores.length && scores[rightIdx] > scores[rightIdx - 1]) {
+    rewards[rightIdx] = rewards[rightIdx - 1] + 1;
+    rightIdx++;
+  }
+}
 exports.minRewards = minRewards;
 
