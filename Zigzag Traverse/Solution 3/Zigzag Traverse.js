45b00562e5cfeb2e8bc1002bed42a7091425ba45
Sejung <kims762@newschool.edu>
2023-03-26 08:26:55 -0400

    Change JavaScript Solution 3 for question Zigzag Traverse

---
+++ Zigzag Traverse/Solution 3/Zigzag Traverse.js
@@ -0,0 +1,38 @@
+function zigzagTraverse(array) {
+  let traverseDown = true;
+  let bottomRow = array.length - 1;
+  let rightCol = array[0].length - 1;
+  let currRow = 0;
+  let currCol = 0;
+  let results = [];
+
+
+    // while (currRow <= bottomRow && currCol <= rightCol) {
+  while (!isOutOfBounds(currRow, bottomRow, currCol, rightCol)) {
+    results.push(array[currRow][currCol]);
+    
+    if (traverseDown) {
+      if (currRow === bottomRow || currCol === 0) { 
+        traverseDown = false;
+        if (currRow === bottomRow) currCol++;
+        else currRow++;
+      } else {
+        currRow++;
+        currCol--;
+      }
+    } else {
+      if (currRow === 0 || currCol === rightCol) {
+        traverseDown = true;
+        if (currCol === rightCol) currRow++;
+        else currCol++;
+      } else {
+        currRow--;
+        currCol++;
+      }
+    }
+  }
+  return results;
+}
+
+
+function isOutOfBounds(row, height, col, width) {
