fc1eb7e382d02c2875e3ac7062bc154c7e2c8de6
Sejung <kims762@newschool.edu>
2023-04-23 11:26:56 -0400

    Change JavaScript Solution 3 for question Best Seat

--- Best Seat/Solution 3/Best Seat.js
+++ Best Seat/Solution 3/Best Seat.js
@@ -1,16 +1,19 @@
 function bestSeat(seats) {
-  let left = 0;
   let bestSeat = -1;
   let largestSpace = 0;
 
 
+  let left = 0;
   while (left < seats.length) {
     let right = left + 1;
-    let availableSpace = 0;
     while (right < seats.length && seats[right] === 0) {
-      availableSpace = right - left;
       right++;
     }
+
+
+    // here we offset by one because in our while loop we will increment one past the last 0 
+    // our right pointer will end up at the first 1 after our 0s which is the condition that broke the loop
+    let availableSpace = right - left -1;
     if (availableSpace > largestSpace) {
       bestSeat = Math.ceil(left + (availableSpace / 2));
       largestSpace = availableSpace;
