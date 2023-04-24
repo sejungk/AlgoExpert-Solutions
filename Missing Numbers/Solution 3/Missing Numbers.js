351dcac141f625006c368ac8896fd74b37af7cf1
Sejung <kims762@newschool.edu>
2023-04-24 10:05:50 -0400

    Change JavaScript Solution 3 for question Missing Numbers

--- Missing Numbers/Solution 3/Missing Numbers.js
+++ Missing Numbers/Solution 3/Missing Numbers.js
@@ -1,10 +1,33 @@
 function missingNumbers(nums) {
-  let includedNums = new Set(nums);
-  let solution = [];
-  
-  for (let num = 1; num < nums.length + 3; num++) {
-    if (!includedNums.has(num)) solution.push(num);
+  let solutionXOR = 0;
+  for (let i = 0; i < nums.length + 3; i++) {
+    solutionXOR ^= i;
+    if (i < nums.length) {
+      solutionXOR ^= nums[i];
+    }
   }
+
+
+  const solution = [0, 0];
+  const setBit = solutionXOR & -solutionXOR;
+  for (let i = 0; i < nums.length + 3; i++) {
+    if ((i & setBit) === 0) {
+      solution[0] ^= i;
+    } else {
+      solution[1] ^= i;
+    }
+
+
+    if (i < nums.length) {
+      if ((nums[i] & setBit) === 0) {
+        solution[0] ^= nums[i];
+      } else {
+        solution[1] ^= nums[i]
+      }
+    }
+  }
+  
+  solution.sort((a, b) => a - b);
   return solution;
 }
 
