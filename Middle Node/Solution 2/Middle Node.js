2acabf46dc3c6a7ab6c2bd5a27227ed7402fe745
Sejung <kims762@newschool.edu>
2023-04-26 19:43:39 -0400

    Change JavaScript Solution 2 for question Middle Node

--- Middle Node/Solution 2/Middle Node.js
+++ Middle Node/Solution 2/Middle Node.js
@@ -13,8 +13,13 @@ exports.LinkedList = LinkedList;
 function middleNode(linkedList) {
   let slowPointer = linkedList;
   let fastPointer = linkedList;
-    
+
+
+  // we must check for fastPointer.next !== null to prevent null errors
+  // we could get an error is fastPointer.next is null and we try to check the next value of null which will throw an error
   while (fastPointer !== null && fastPointer.next !== null) {
+    // fastPointer will move twice as fast or double as slow pointer
+    // so for each one node slowPointer moves, fastPointer will move double which is 2 nodes
     slowPointer = slowPointer.next;
     // .next.next will not throw an error bc we know fastPointer.next is not null
     fastPointer = fastPointer.next.next;
