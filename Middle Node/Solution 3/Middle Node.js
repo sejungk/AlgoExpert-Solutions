23836f7d08e24562797856e3dd566a14f4ece9dc
Sejung <kims762@newschool.edu>
2023-04-26 19:37:55 -0400

    Create JavaScript Solution 3 for question Middle Node

---
+++ Middle Node/Solution 3/Middle Node.js
@@ -0,0 +1,32 @@
+// This is an input class. Do not edit.
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+
+exports.LinkedList = LinkedList;
+
+
+function middleNode(linkedList) {
+  let count = 0;
+  let currNode = linkedList;
+  
+  while (currNode.next !== null ) {
+    count++;
+    currNode = currNode.next;
+  }
+  
+  let middleNode = linkedList;
+  for (let i = 0; i < Math.ceil(count / 2); i++) {
+    middleNode = middleNode.next;
+  }
+  return middleNode;
+}
+
+
+// Do not edit the line below.
+exports.middleNode = middleNode;
+
