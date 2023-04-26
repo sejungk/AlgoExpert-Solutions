f41824911ef9e9d8a9940aea6b02ae4570f5dc58
Sejung <kims762@newschool.edu>
2023-04-26 19:38:17 -0400

    Change JavaScript Solution 3 for question Middle Node

--- Middle Node/Solution 3/Middle Node.js
+++ Middle Node/Solution 3/Middle Node.js
@@ -14,13 +14,13 @@ function middleNode(linkedList) {
   let count = 0;
   let currNode = linkedList;
   
-  while (currNode.next !== null ) {
+  while (currNode !== null ) {
     count++;
     currNode = currNode.next;
   }
   
   let middleNode = linkedList;
-  for (let i = 0; i < Math.ceil(count / 2); i++) {
+  for (let i = 0; i < Math.floor(count / 2); i++) {
     middleNode = middleNode.next;
   }
   return middleNode;
