95fd15c710b0a264da79b86d9e38d7d5efcc02de
Sejung <kims762@newschool.edu>
2023-04-30 10:13:23 -0400

    Change JavaScript Solution 1 for question Merging Linked Lists

--- Merging Linked Lists/Solution 1/Merging Linked Lists.js
+++ Merging Linked Lists/Solution 1/Merging Linked Lists.js
@@ -11,42 +11,19 @@ exports.LinkedList = LinkedList;
 
 
 function mergingLinkedLists(linkedListOne, linkedListTwo) {
+  let listOneNodes = new Set();
   let nodeOne = linkedListOne;
-  let nodeTwo = linkedListTwo;
-
-
-  // this will not be an indefinite loop because by the time we looped twice,
-  // if there is not intersection, we will end up at null values at the same time
-  // since we iterated through same # of node, this will break the loop
-  // since both nodes will be equal to null
-  
-  while (nodeOne !== nodeTwo) {
-    if (nodeOne === null) nodeOne = linkedListTwo;
-    else nodeOne = nodeOne.next;
-
-
-    if (nodeTwo === null) nodeTwo = linkedListOne;
-    else nodeTwo = nodeTwo.next;
+  while (nodeOne !== null) {
+    listOneNodes.add(nodeOne);
+    nodeOne = nodeOne.next;
   }
-  return nodeOne;
+
+
+  let nodeTwo = linkedListTwo;
+  while (nodeTwo !== null) {
+    if (listOneNodes.has(nodeTwo)) return nodeTwo;
+    nodeTwo = nodeTwo.next;
+  }
+  return null;
 }
 
-
-// Do not edit the line below.
-exports.mergingLinkedLists = mergingLinkedLists;
-
-
-// // This is an input class. Do not edit.
-// class LinkedList {
-//   constructor(value) {
-//     this.value = value;
-//     this.next = null;
-//   }
-// }
-
-
-// exports.LinkedList = LinkedList;
-
-
-// function mergingLinkedLists(linkedListOne, linkedListTwo) {
-//   let listOneNodes = new Set();
