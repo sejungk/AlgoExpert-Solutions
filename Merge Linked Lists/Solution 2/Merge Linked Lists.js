7a7c0743b13c4b38d779c43487d258b13230bb19
Sejung <kims762@newschool.edu>
2023-05-01 17:36:44 -0400

    Change JavaScript Solution 2 for question Merge Linked Lists

--- Merge Linked Lists/Solution 2/Merge Linked Lists.js
+++ Merge Linked Lists/Solution 2/Merge Linked Lists.js
@@ -8,24 +8,26 @@ class LinkedList {
 
 
 function mergeLinkedLists(headOne, headTwo) {
-  let p1 = headOne;
-  let p1Prev = null;
-  let p2 = headTwo;
-
-
-  while (p1 !== null && p2 !== null) {
-    if (p1.value < p2.value) {
-      p1Prev = p1;
-      p1 = p1.next;
-    } else {
-      if (p1Prev !== null) p1Prev.next = p2;
-      p1Prev = p2;
-      p2 = p2.next;
-      p1Prev.next = p1;
-    }
-  }
-  if (p1 === null) p1Prev.next = p2;
+  mergeLinkedList(null, headOne, headTwo);
   return headOne.value < headTwo.value ? headOne : headTwo;
+}
+
+
+function mergeLinkedList(prev, p1, p2) {
+  if (p1 === null) {
+    prev.next = p2;
+    return;
+  }
+  if (p2 === null) return;
+  
+  if (p1.value < p2.value) {
+    mergeLinkedList(p1, p1.next, p2);
+  } else {
+    if (prev !== null) prev.next = p2;
+    const temp = p2.next;
+    p2.next = p1;      
+    mergeLinkedList(p2, p1, temp);
+  }
 }
 
 
