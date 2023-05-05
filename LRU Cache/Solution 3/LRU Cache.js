3a066da91a23609f0a1edf7598ca9396cef2d9da
Sejung <kims762@newschool.edu>
2023-05-05 10:28:10 -0400

    Create JavaScript Solution 3 for question LRU Cache

---
+++ LRU Cache/Solution 3/LRU Cache.js
@@ -0,0 +1,63 @@
+// Do not edit the class below except for the insertKeyValuePair,
+// getValueFromKey, and getMostRecentKey methods. Feel free
+// to add new properties and methods to the class.
+class LRUCache {
+  constructor(maxSize) {
+    this.cache = {};
+    this.currentSize = 0;
+    this.maxSize = maxSize || 1;
+    this.keyValueList = new DoublyLinkedList();
+  }
+
+
+  insertKeyValuePair(key, value) {
+    if (!this.cache[key]) {
+      if (this.currentSize === this.maxSize) this.evictLeastRecent();
+      else this.currentSize++;
+      this.cache[key] = new DoublyLinkedListNode(key, value);
+    } else {
+      this.replaceKey(key, value);
+    }
+    this.updateMostRecent(this.cache[key]);
+  }
+  
+
+
+  getValueFromKey(key) {
+    if (!this.cache[key]) return null;
+    this.updateMostRecent(this.cache[key]);
+    return this.cache[key].value;
+  }
+
+
+  getMostRecentKey() {
+    if (this.keyValueList.head === null) return;
+    return this.keyValueList.head.key;
+  }
+  
+  evictLeastRecent() {
+    let keyToRemove = this.keyValueList.tail.key;
+    this.keyValueList.removeTail();
+    delete this.cache[keyToRemove];
+  }
+
+
+  replaceKey(key, value) {
+    if (!this.cache[key]) throw new Error("key not found");
+    else this.cache[key].value = value;
+  }
+
+
+  updateMostRecent(node) {
+    this.keyValueList.setHeadTo(node);
+  }
+}
+
+
+
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
