7032937c566d5f08f1bb4941fa3e587fb856d7ae
Sejung <kims762@newschool.edu>
2023-05-13 11:10:44 -0400

    Create JavaScript Solution 1 for question Evaluate Expression Tree

---
+++ Evaluate Expression Tree/Solution 1/Evaluate Expression Tree.js
@@ -0,0 +1,33 @@
+// This is an input class. Do not edit.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+
+function evaluateExpressionTree(tree) {
+  // breadth first approach
+  // calc all pairs level by level
+  // keep track of calcs for each branch
+  if (tree.value >= 0) return tree.value;
+
+
+  const leftValue = evaluateExpressionTree(tree.left);
+  const rightValue = evaluateExpressionTree(tree.right);
+
+
+  if (tree.value === -1) return leftValue + rightValue;
+  if (tree.value === -2) return leftValue - rightValue;
+  if (tree.value === -3) return Math.trunc(leftValue / rightValue);
+  
+  return leftValue * rightValue;
+}
+
+
+// Do not edit the lines below.
+exports.BinaryTree = BinaryTree;
+exports.evaluateExpressionTree = evaluateExpressionTree;
+
