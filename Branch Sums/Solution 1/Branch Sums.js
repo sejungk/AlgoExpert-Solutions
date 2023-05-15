61998f0094285ef119609e3bd873a6f469f0948f
Sejung <kims762@newschool.edu>
2023-05-15 07:47:06 -0400

    Create JavaScript Solution 1 for question Branch Sums

---
+++ Branch Sums/Solution 1/Branch Sums.js
@@ -0,0 +1,32 @@
+// This is the class of the input root.
+// Do not edit it.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+
+function branchSums(root) {
+  let sums = [];
+  calcBranch(sums, root, 0);
+  return sums;
+}
+
+
+function calcBranch(array, node, runningSum) {
+  if (!node) return;
+  runningSum = node.value + runningSum;
+  if (node.left === null && node.right === null) {
+    array.push(runningSum);
+    return;
+  }
+  calcBranch(array, node.left, runningSum);
+  calcBranch(array, node.right, runningSum);
+}
+// Do not edit the lines below.
+exports.BinaryTree = BinaryTree;
+exports.branchSums = branchSums;
+
