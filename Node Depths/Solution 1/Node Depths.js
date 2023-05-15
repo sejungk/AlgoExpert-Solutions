ec79c159cbf8985f385dd107933026e5a505b638
Sejung <kims762@newschool.edu>
2023-05-15 08:29:35 -0400

    Change JavaScript Solution 1 for question Node Depths

--- Node Depths/Solution 1/Node Depths.js
+++ Node Depths/Solution 1/Node Depths.js
@@ -1,9 +1,22 @@
-function nodeDepths(root, depth = 0) {
-  if (!root) return 0;
-  return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1);
+// function nodeDepths(root, depth = 0) {
+//   if (!root) return 0;
+//   return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1);
+// }
+
+
+function nodeDepths(root) {
+  let totalDepth = 0;
+  let stack = [{node: root, depth: 0}]
+  
+  while (stack.length) {
+    let {node, depth} = stack.pop();
+    if (!node) continue;
+    totalDepth = totalDepth + depth;
+    stack.push({node: node.left, depth: depth + 1});
+    stack.push({node: node.right, depth: depth + 1});
+  }
+  return totalDepth;
 }
-
-
 // This is the class of the input binary tree.
 class BinaryTree {
   constructor(value) {
