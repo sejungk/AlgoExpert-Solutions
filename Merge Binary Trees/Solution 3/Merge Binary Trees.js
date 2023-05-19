ae73efe2a2acc9a90acc2595cb97bb3fc6081a5d
Sejung <kims762@newschool.edu>
2023-05-19 08:20:31 -0400

    Create JavaScript Solution 3 for question Merge Binary Trees

---
+++ Merge Binary Trees/Solution 3/Merge Binary Trees.js
@@ -0,0 +1,28 @@
+// This is an input class. Do not edit.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+
+exports.BinaryTree = BinaryTree;
+
+
+function mergeBinaryTrees(tree1, tree2) {
+  if (!tree1) return tree2;
+  if (!tree2) return tree1;
+
+
+  tree1.value += tree2.value;
+  tree1.left = mergeBinaryTrees(tree1.left, tree2.left);
+  tree1.right = mergeBinaryTrees(tree1.right, tree2.right);
+  return tree1;
+}
+
+
+// Do not edit the line below.
+exports.mergeBinaryTrees = mergeBinaryTrees;
+
