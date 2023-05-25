2e44cf0b3aef226a4f2d9724ee4154deb83def99
Sejung <kims762@newschool.edu>
2023-05-25 10:07:23 -0400

    Create JavaScript Solution 1 for question Lowest Common Manager

---
+++ Lowest Common Manager/Solution 1/Lowest Common Manager.js
@@ -0,0 +1,32 @@
+function getLowestCommonManager(topManager, reportOne, reportTwo) {
+  return getOrgInfo(topManager, reportOne, reportTwo).lowestCommonManager;
+}
+
+
+function getOrgInfo(manager, reportOne, reportTwo) {
+  let numImportantReports = 0;
+  for (const directReports of manager.directReports) {
+    const orgInfo = getOrgInfo(directReports, reportOne, reportTwo);
+    if (!!orgInfo.lowestCommonManager) return orgInfo;
+    numImportantReports += orgInfo.numImportantReports;
+  }
+  // there will always come a point where we reach a subtree that rooted at one of our 2 reports
+  // base case
+  if (manager === reportOne || manager === reportTwo) numImportantReports++;
+  const lowestCommonManager = numImportantReports === 2 ? manager : null;
+  return {lowestCommonManager, numImportantReports};
+}
+
+
+// This is an input class. Do not edit.
+class OrgChart {
+  constructor(name) {
+    this.name = name;
+    this.directReports = [];
+  }
+}
+
+
+// Do not edit the line below.
+exports.getLowestCommonManager = getLowestCommonManager;
+
