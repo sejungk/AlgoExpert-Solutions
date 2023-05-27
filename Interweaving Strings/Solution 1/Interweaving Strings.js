3a9f7220b67ad1db94a3fb6edb473a679e723c94
Sejung <kims762@newschool.edu>
2023-05-27 08:51:26 -0400

    Change JavaScript Solution 1 for question Interweaving Strings

--- Interweaving Strings/Solution 1/Interweaving Strings.js
+++ Interweaving Strings/Solution 1/Interweaving Strings.js
@@ -1,27 +1,32 @@
 function interweavingStrings(one, two, three) {
   if (one.length + two.length !== three.length) return false;
-  let matrix = new Array(one.length).fill(0).map(_ => new Array(two.length).fill(0));
-  return areInterwoven(one, two, three, 0, 0, matrix);
+  let cache = new Array(one.length + 1).fill(0).map(_ => new Array(two.length + 1).fill(null));
+  return areInterwoven(one, two, three, 0, 0, cache);
 }
 
 
-function areInterwoven(one, two, three, i, j) {
-  
+function areInterwoven(one, two, three, i, j, cache) {
+  if (cache[i][j] !== null) return cache[i][j];
+ 
   const k = i + j;
   // we've brought i and j to the end of their respective strings
   if (k === three.length) return true;
 
 
   if (i < one.length && one[i] === three[k]) {
-    if (areInterwoven(one, two, three, i + 1, j)) return true;
+    cache[i][j] = areInterwoven(one, two, three, i + 1, j, cache);
+    if (cache[i][j]) return true;
   }
 
 
   if (j < two.length && two[j] === three[k]) {
-    return areInterwoven(one, two, three, i, j + 1);
+    cache[i][j] = areInterwoven(one, two, three, i, j + 1, cache);
+    return cache[i][j];
   }
-  
-  return false;
+
+
+  cache[i][j] = false;
+  return cache[i][j];
 }
 
 
