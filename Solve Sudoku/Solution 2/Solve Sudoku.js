c0cc31fe40142a48f07179879ed1de9da3ea58ae
Sejung <kims762@newschool.edu>
2023-05-27 14:13:27 -0400

    Change JavaScript Solution 2 for question Solve Sudoku

--- Solve Sudoku/Solution 2/Solve Sudoku.js
+++ Solve Sudoku/Solution 2/Solve Sudoku.js
@@ -14,24 +14,38 @@ function solvePartialSudoku(row, col, board) {
   let currCol = col;
 
 
+  // if the col is at the end board, 
+  // we want to reset col to 0 and move down a row
   if (currCol === board[currRow].length) {
     currRow++;
     currCol = 0;
+    // if we're at the last square, we have solved the problem
     if (currRow === board.length) return true;
   }
 
 
+  // check if the current position is an empty square
   if (board[currRow][currCol] === 0) {
+    // func that try all posible digits (1-9) at this position
     return tryDigitsAtPosition(currRow, currCol, board);
   }
+  // if were at a position that is not an empty square, 
+  // move over to the next square (next col)
   return solvePartialSudoku(currRow, currCol + 1, board);
 }
 
 
+
+
 function tryDigitsAtPosition(row, col, board) {
+  // loop through all digits 1 - 9
   for (let digit = 1; digit < 10; digit++) {
+    // check if this digit I'm trying is valid at position
     if (isValidAtPosition(digit, row, col, board)) {
+      // temporarily insert digit into position if digit is valid
       board[row][col] = digit;
+      // continue to solve rest of board, and if the board was solved 
+      // with the prev inserted digit, return true
       if (solvePartialSudoku(row, col + 1, board)) return true;
     }
   }
