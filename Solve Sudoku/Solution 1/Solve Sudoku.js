8c2cef0e2e009f10490f7bbbce5815d42e5d3296
Sejung <kims762@newschool.edu>
2023-05-29 15:31:01 -0400

    Create JavaScript Solution 1 for question Solve Sudoku

---
+++ Solve Sudoku/Solution 1/Solve Sudoku.js
@@ -0,0 +1,66 @@
+function solveSudoku(board) {
+  solveBoard(board, 0, 0);
+  return board;
+}
+
+
+function solveBoard(board, row, col) {
+  let currCol = col;
+  let currRow = row;
+
+
+  if (currCol === board[currRow].length) {
+    currRow++;
+    currCol = 0;
+    if (currRow === board.length) return true;
+  }
+
+
+  if (board[currRow][currCol] === 0) {
+    return tryDigitsAtPosition(board, currCol, currRow);
+  }
+  return solveBoard(board, currRow, currCol + 1);
+}
+
+
+function tryDigitsAtPosition(board, col, row) {
+  for (let digit = 1; digit < 10; digit++) {
+    if (isDigitValid(board, row, col, digit)) {
+      board[row][col] = digit;
+      if (solveBoard(board, row, col + 1)) return true;
+    }
+  }
+  board[row][col] = 0;
+  return false;
+}
+
+
+function isDigitValid(board, row, col, digit) {
+  const isRowValid = !board[row].includes(digit);
+  const isColValid = !board.map(r => r[col]).includes(digit);
+
+
+  if (!isRowValid || !isColValid) return false;
+
+
+  const rowStartIdx = Math.floor(row / 3) * 3;
+  const colStartIdx = Math.floor(col / 3) * 3;
+
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      const currRow = rowStartIdx + i;
+      const currCol = colStartIdx + j;
+      const currSquare = board[currRow][currCol];
+
+
+      if (currSquare === digit) return false;  
+    }
+  }
+  return true;
+}
+
+
+// Do not edit the line below.
+exports.solveSudoku = solveSudoku;
+
