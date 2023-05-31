fa7d03725225d8750cee9ffd4446776c0db81ca4
Sejung <kims762@newschool.edu>
2023-05-31 18:33:25 -0400

    Create JavaScript Solution 2 for question Number Of Binary Tree Topologies

---
+++ Number Of Binary Tree Topologies/Solution 2/Number Of Binary Tree Topologies.js
@@ -0,0 +1,16 @@
+function numberOfBinaryTreeTopologies(n) {
+  if (n === 0) return 1;
+  let numberOfTrees = 0;
+  for (let leftTreeSize = 0; leftTreeSize < n; leftTreeSize++) {
+    const rightTreeSize = n - 1 - leftTreeSize;
+    const numberOfLeftTrees = numberOfBinaryTreeTopologies(leftTreeSize);
+    const numberOfRightTrees = numberOfBinaryTreeTopologies(rightTreeSize);
+    numberOfTrees += numberOfLeftTrees * numberOfRightTrees;
+  }
+  return numberOfTrees;
+}
+
+
+// Do not edit the line below.
+exports.numberOfBinaryTreeTopologies = numberOfBinaryTreeTopologies;
+
