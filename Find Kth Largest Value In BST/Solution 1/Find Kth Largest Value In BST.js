e2cfaf6bdf0d0c5fdbe4ebf8722972dcfc839947
Sejung <kims762@newschool.edu>
2023-05-31 10:26:25 -0400

    Create JavaScript Solution 1 for question Find Kth Largest Value In BST

---
+++ Find Kth Largest Value In BST/Solution 1/Find Kth Largest Value In BST.js
@@ -0,0 +1,33 @@
+// This is an input class. Do not edit.
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+
+
+
+function findKthLargestValueInBst(tree, k) {
+	const sortedNodes = [];
+  inorderTraverse(tree, sortedNodes);
+	return sortedNodes[sortedNodes.length - k];
+}
+
+
+function inorderTraverse(currentNode, sortedNodes) {
+	
+	if(currentNode === null) return;
+		
+		inorderTraverse(currentNode.left, sortedNodes);
+		sortedNodes.push(currentNode.value);
+		inorderTraverse(currentNode.right, sortedNodes);
+}
+
+
+// Do not edit the lines below.
+exports.BST = BST;
+exports.findKthLargestValueInBst = findKthLargestValueInBst;
+
