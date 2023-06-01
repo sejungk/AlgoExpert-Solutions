3aa51581ebc04f1a0a2c282bd3b7efe584fdc1bf
Sejung <kims762@newschool.edu>
2023-06-01 10:04:58 -0400

    Change JavaScript Solution 1 for question Number Of Binary Tree Topologies

--- Number Of Binary Tree Topologies/Solution 1/Number Of Binary Tree Topologies.js
+++ Number Of Binary Tree Topologies/Solution 1/Number Of Binary Tree Topologies.js
@@ -1,13 +1,29 @@
 // time: catalan number space: catalan number
-function numberOfBinaryTreeTopologies(n) {
-  if (n === 0) return 1;
+// function numberOfBinaryTreeTopologies(n) {
+//   if (n === 0) return 1;
+//   let numberOfTrees = 0;
+//   for (let leftTreeSize = 0; leftTreeSize < n; leftTreeSize++) {
+//     const rightTreeSize = n - 1 - leftTreeSize;
+//     const numberOfLeftTrees = numberOfBinaryTreeTopologies(leftTreeSize);
+//     const numberOfRightTrees = numberOfBinaryTreeTopologies(rightTreeSize);
+//     numberOfTrees += numberOfLeftTrees * numberOfRightTrees;
+//   }
+//   return numberOfTrees;
+// }
+
+
+function numberOfBinaryTreeTopologies(n, cache = {0: 1}) {
+  if (n in cache) return cache[n];
+
+
   let numberOfTrees = 0;
-  for (let leftTreeSize = 0; leftTreeSize < n; leftTreeSize++) {
-    const rightTreeSize = n - 1 - leftTreeSize;
-    const numberOfLeftTrees = numberOfBinaryTreeTopologies(leftTreeSize);
-    const numberOfRightTrees = numberOfBinaryTreeTopologies(rightTreeSize);
+  for (let left = 0; left < n; left++) {
+    let right = n - left - 1;
+    const numberOfLeftTrees = numberOfBinaryTreeTopologies(left, cache);
+    const numberOfRightTrees = numberOfBinaryTreeTopologies(right, cache);
     numberOfTrees += numberOfLeftTrees * numberOfRightTrees;
   }
+  cache[n] = numberOfTrees;
   return numberOfTrees;
 }
 
