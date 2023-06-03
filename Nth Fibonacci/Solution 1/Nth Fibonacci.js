dad5f2f407bc45024d89bf5d31e96a8e9d409328
Sejung <kims762@newschool.edu>
2023-06-03 12:14:06 -0400

    Change JavaScript Solution 1 for question Nth Fibonacci

--- Nth Fibonacci/Solution 1/Nth Fibonacci.js
+++ Nth Fibonacci/Solution 1/Nth Fibonacci.js
@@ -1,40 +1,31 @@
 // function getNthFib(n) {
 //   if (n === 1) return 0;
 //   if (n === 2) return 1;
-
-
-//   return getNthFib(n-1) + getNthFib(n-2);
+  
+//   return getNthFib(n - 1) + getNthFib(n - 2);
 // }
 
 
-// function getNthFib(n, memo = {1: 0, 2: 1}) {
-//   // we need to use n in memo and not memo[n] because...
-//   // memo[1] = 0 which will return false
-//   if (n in memo) return memo[n];
-//   else {
-//     memo[n] = getNthFib(n-1, memo) + getNthFib(n-2, memo);
-//     return memo[n];
-//   }
+// function getNthFib(n, cache = {1: 0, 2: 1}) {
+//   if (n in cache) return cache[n];
+  
+//   cache[n] = getNthFib(n - 1, cache) + getNthFib(n - 2, cache);
+//   return cache[n];
 // }
 
 
-function getNthFib(n, memo = {1: 0, 2: 1}) {
-  let prev2Nums = [0, 1];
-
-
-  if (n === 1) return 0;
-  if (n === 2) return 1;
+function getNthFib(n, prevFibs = [0, 1]) {
+  if (n < 3) return n - 1;
   
-  let currNum = 3;
-  
-  while (currNum <= n) {
-    let newSum = prev2Nums[0] + prev2Nums[1];
-    prev2Nums[0] = prev2Nums[1];
-    prev2Nums[1] = newSum;
-    currNum++;
+  for (let i = 2; i < n; i++) {
+    let newFib = prevFibs[0] + prevFibs[1];
+    prevFibs[0] = prevFibs[1];
+    prevFibs[1] = newFib;
   }
-  return prev2Nums[1];
+  return prevFibs[1];
 }
+
+
 // Do not edit the line below.
 exports.getNthFib = getNthFib;
 
