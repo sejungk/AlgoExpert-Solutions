717f6c11aec4314b14ec67d64506d3522f3be5a9
Sejung <kims762@newschool.edu>
2023-06-04 16:18:56 -0400

    Change JavaScript Solution 3 for question Invert Binary Tree

--- Invert Binary Tree/Solution 3/Invert Binary Tree.js
+++ Invert Binary Tree/Solution 3/Invert Binary Tree.js
@@ -1,9 +1,29 @@
-function invertBinaryTree(tree) {
-  if (tree === null) return;
-  swap(tree);  
+// function invertBinaryTree(tree) {
+//   if (tree === null) return;
+//   swap(tree);  
   
-  invertBinaryTree(tree.left);
-  invertBinaryTree(tree.right);
+//   invertBinaryTree(tree.left);
+//   invertBinaryTree(tree.right);
+// }
+
+
+// function swap(tree) {
+//   let temp = tree.left;
+//   tree.left = tree.right;
+//   tree.right = temp;
+// }
+
+
+function invertBinaryTree(tree) {
+  let queue = [tree];
+  
+  while (queue.length) {
+    const node = queue.shift();
+    if (node === null) continue;
+    swap(node);
+    queue.push(node.left);
+    queue.push(node.right);
+  }
 }
 
 
