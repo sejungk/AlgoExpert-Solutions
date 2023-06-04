d2f5436d1a3dcd4a6f00f74965d3df7e75a72119
Sejung <kims762@newschool.edu>
2023-06-04 08:58:14 -0400

    Create JavaScript Solution 1 for question Symmetrical Tree

---
+++ Symmetrical Tree/Solution 1/Symmetrical Tree.js
@@ -0,0 +1,30 @@
+// This is an input class. Do not edit.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+
+exports.BinaryTree = BinaryTree;
+
+
+function symmetricalTree(tree) {
+  return traverseTree(tree.left, tree.right);
+}
+
+
+function traverseTree(leftNode, rightNode) {
+  if (leftNode === null && rightNode === null) return true;
+  if (leftNode === null || rightNode === null) return false;
+  if (leftNode.value !== rightNode.value) return false;
+  
+  return traverseTree(leftNode.left, rightNode.right) && traverseTree(rightNode.left, leftNode.right);
+}
+
+
+// Do not edit the line below.
+exports.symmetricalTree = symmetricalTree;
+
