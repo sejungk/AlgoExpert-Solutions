41c14d62a02c5e118a3d5a5f94110d7b8e39310b
Sejung <kims762@newschool.edu>
2023-06-04 15:50:42 -0400

    Change JavaScript Solution 3 for question Node Depths

--- Node Depths/Solution 3/Node Depths.js
+++ Node Depths/Solution 3/Node Depths.js
@@ -4,9 +4,18 @@
 // }
 
 
-function nodeDepths(root, depth = 0) {
-  if (root === null) return 0;
-  return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1)
+function nodeDepths(root) {
+  let stack = [{node: root, depth: 0}];
+  let depthSum = 0;
+  
+  while (stack.length) {
+    let {node, depth} = stack.pop();
+    depthSum += depth;
+    
+    if (node.left) stack.push({node: node.left, depth: depth+ 1});
+    if (node.right) stack.push({node: node.right, depth: depth + 1});
+  }
+  return depthSum;
 }
 
 
