2b70fa36fa847ffd0f3adfe1e80b5b1e56510f35
Sejung <kims762@newschool.edu>
2023-06-08 19:50:18 -0400

    Change JavaScript Solution 2 for question Find Nodes Distance K

--- Find Nodes Distance K/Solution 2/Find Nodes Distance K.js
+++ Find Nodes Distance K/Solution 2/Find Nodes Distance K.js
@@ -1,40 +1,61 @@
-// This is an input class. Do not edit.
-class BinaryTree {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
-// start breadth first search at target node
-// find all parent nodes
-// find target node
-// perform breadth first search and find all nodes that are at distance k
 
 
 function findNodesDistanceK(tree, target, k) {
-  const nodesToParents = {};
-  populateNodesToParents(tree, nodesToParents);
-  const targetNode = getNodeFromValue(target, tree, nodesToParents);
-
-
-  return breadthFirstSearchForNodesDistanceK(targetNode, nodesToParents, k);
+  const nodesDistanceK = [];
+  findDistanceFromNodeToTarget(tree, target, k, nodesDistanceK);
+  return nodesDistanceK;
 }
 
 
-function breadthFirstSearchForNodesDistanceK(targetNode, nodesToParents, k) {
-  const queue = [[targetNode, 0]];
-  const seen = new Set([targetNode.value]);
-  while (queue.length > 0) {
-    const [currentNode, distanceFromTarget] = queue.shift();
+function findDistanceFromNodeToTarget(node, target, k, nodesDistanceK) {
+  // 4 recursive cases we want to consider: 
+    // 1) target node is in left subtree
+    // 2) target node is in right subtree
+    // 3) curr node is target node
+    // 4) target node is not in any of our subtrees
 
 
-    if (distanceFromTarget === k) {
-      const nodesDistanceK = queue.map(pair => pair[0].value);
-      nodesDistanceK.push(currentNode.value);
-      return nodesDistanceK;
-    }
+  // -1 represents that the target is not in this subtree
+  if (node === null) return -1;
 
 
-    const connectedNodes = [currentNode.left, currentNode.right, nodesToParents[currentNode.value]];
-    for (const node of connectedNodes) {
+  // if we find target node return 1
+  if (node.value === target) {
+    addSubtreeNodeAtDistanceK(node, 0, k, nodesDistanceK);
+    return 1;
+  }
+  const leftDistance = findDistanceFromNodeToTarget(node.left, target, k, nodesDistanceK);
+  const rightDistance = findDistanceFromNodeToTarget(node.right, target, k, nodesDistanceK);
+
+
+  if (leftDistance === k || rightDistance === k) nodesDistanceK.push(node.value);
+
+
+  if (leftDistance !== -1) {
+    addSubtreeNodeAtDistanceK(node.right, leftDistance + 1, k, nodesDistanceK);
+    return leftDistance + 1;
+  }
+
+
+  if (rightDistance !== -1) {
+    addSubtreeNodeAtDistanceK(node.left, rightDistance + 1, k, nodesDistanceK);
+    return rightDistance + 1;
+  }
+  return -1
+}
+
+
+function addSubtreeNodeAtDistanceK(node, distance, k, nodesDistanceK) {
+  if (node === null) return;
+  if (distance === k) nodesDistanceK.push(node.value);
+  else {
+    addSubtreeNodeAtDistanceK(node.left, distance + 1, k, nodesDistanceK);
+    addSubtreeNodeAtDistanceK(node.right, distance + 1, k, nodesDistanceK);
+  }
+}
+
+
+// Do not edit the lines below.
+exports.BinaryTree = BinaryTree;
+exports.findNodesDistanceK = findNodesDistanceK;
+
