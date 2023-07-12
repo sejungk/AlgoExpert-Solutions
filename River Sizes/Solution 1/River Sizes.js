299ef2d6a24c1d7ff83620f3974dccc3ac719195
Sejung <kims762@newschool.edu>
2023-07-12 11:33:13 -0400

    Change JavaScript Solution 1 for question River Sizes

--- River Sizes/Solution 1/River Sizes.js
+++ River Sizes/Solution 1/River Sizes.js
@@ -1,40 +1,38 @@
 function riverSizes(matrix) {
-	let sizes = [];
-	let visited = matrix.map(row => row.map(value => false));
-		for (let i = 0; i < matrix.length; i++) {
-			for (let j = 0; j < matrix[i].length; j++) {
-				if (visited[i][j]) continue;
-			  traverseNode(sizes, visited, matrix, i, j)
-			}
-		}
-	return sizes
-	}
-
-
-
-
-function traverseNode(sizes, visited, matrix, i , j) {
-	let riverSize = 0;
-	let nodesToExplore = [[i, j]];
-	while (nodesToExplore.length) {
-		const currNode = nodesToExplore.pop();
-		i = currNode[0];
-		j = currNode[1];
-		if (visited[i][j]) continue;
-		visited[i][j] = true;
-		if (matrix[i][j] === 0) continue;
-		riverSize++;
-		const unvisitedNeighbors = getUnvisitedNeighbors(i, j, matrix, visited);
-		for (const neighbor of unvisitedNeighbors) {
-			nodesToExplore.push(neighbor)
-		}
-	}
-	if (riverSize > 0) sizes.push(riverSize);
+  let sizes = [];
+  let visited = new Set();
+  
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[0].length; col++) {
+      let size = explore(row, col, matrix, visited, sizes);
+      if (size > 0) sizes.push(size);
+    }
+  }
+  return sizes;
 }
 
 
-function getUnvisitedNeighbors(i, j, matrix, visited) {
-	const unvisitedNeighbors = [];
-	if (i > 0 && !visited[i - 1][j]) unvisitedNeighbors.push([i - 1, j]);
-	if (i < matrix.length - 1 && !visited[i + 1][j]) unvisitedNeighbors.push([i + 1, j]);
-	if (j > 0 && !visited[i][j - 1]) unvisitedNeighbors.push([i, j - 1]);
+function explore(row, col, matrix, visited, sizes) {
+  let key = row + ',' + col;
+  if (visited.has(key)) return 0;
+  visited.add(key);
+  if (!inBounds(row, col, matrix) || matrix[row][col] === 0) return 0;
+console.log(row, col)
+  let count = 1;
+  let deltas =[ [0, 1], [1, 0], [-1, 0], [0, -1] ];
+  for (let delta of deltas) {
+    const [ deltaRow, deltaCol ] =  delta;
+    count += explore(row + deltaRow, col + deltaCol, matrix, visited, sizes);
+  }
+  return count;
+}
+
+
+function inBounds(row, col, matrix) {
+  const rowInBounds = row >= 0 && row < matrix.length;
+  const colInBounds = col >= 0 && col < matrix[0].length;
+  return rowInBounds && colInBounds;
+}
+// Do not edit the line below.
+exports.riverSizes = riverSizes;
+
