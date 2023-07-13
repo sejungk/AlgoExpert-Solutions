17e0cf3086d746e653812ce0cbc8b4a16036d520
Sejung <kims762@newschool.edu>
2023-07-13 14:39:55 -0400

    Create JavaScript Solution 1 for question Two-Colorable

---
+++ Two-Colorable/Solution 1/Two-Colorable.js
@@ -0,0 +1,30 @@
+function twoColorable(edges) {
+  const colorAssignment = {};
+  
+  for (let node in edges) {
+    if (!(node in colorAssignment)) {
+      if (colorNodes(node, edges, true, colorAssignment) === false) return false;
+    }
+  }
+  return true;
+}
+
+
+function colorNodes(node, edges, color, colorAssignment) {
+  if (node in colorAssignment) {
+    if (colorAssignment[node] !== color) return false;
+    else return true;
+  }
+  
+  colorAssignment[node] = color;
+  for (let child of edges[node]) {
+    if (colorNodes(child, edges, !color, colorAssignment) === false) return false;
+  }
+  
+  return true;
+}
+
+
+// Do not edit the line below.
+exports.twoColorable = twoColorable;
+
