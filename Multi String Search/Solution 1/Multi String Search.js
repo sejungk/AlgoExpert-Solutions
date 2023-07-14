35c76aecfdf3f40728aa411efafdb33138034129
Sejung <kims762@newschool.edu>
2023-07-14 11:55:33 -0400

    Create JavaScript Solution 1 for question Multi String Search

---
+++ Multi String Search/Solution 1/Multi String Search.js
@@ -0,0 +1,57 @@
+class createTrie {
+  constructor(sentence) {
+    this.root = {};
+    this.endSymbol = '*';
+    this.populateTrieWithWords(sentence);
+  }
+
+
+  populateTrieWithWords(sentence) {
+    const words = sentence.split(" ");
+
+
+    for (let word of words) {
+      for (let i = 0; i < word.length; i++) {
+        this.addNodesStartingAt(word, i);
+      }
+    }
+  }
+
+
+  addNodesStartingAt(word, i) {
+    let node = this.root;
+    for (let j = i; j < word.length; j++) {
+      let letter = word[j];
+      if (!(letter in node)) node[letter] = {};
+      node = node[letter];
+    }
+    node[this.endSymbol] = true;
+  }
+  
+  search(string) {
+    let node = this.root;
+    for (let letter of string) {
+      if (!(letter in node)) return false;
+      node = node[letter];
+    }
+    return true;
+    // return (this.endSymbol in node);
+  }
+}
+
+
+function multiStringSearch(bigString, smallStrings) {
+  const trie = new createTrie(bigString);
+  const foundStrings = new Array(smallStrings.length).fill(false);
+  
+  for (let i = 0; i < smallStrings.length; i++) {
+    let word = smallStrings[i];
+    if (trie.search(word) === true) foundStrings[i] = true;
+  }
+  return foundStrings;
+}
+
+
+// Do not edit the line below.
+exports.multiStringSearch = multiStringSearch;
+
