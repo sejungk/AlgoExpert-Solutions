b0c4521f977cfe450a58ec68ba24ff57b9242ecf
Sejung <kims762@newschool.edu>
2023-07-15 18:17:08 -0400

    Change JavaScript Solution 2 for question Largest Island

--- Largest Island/Solution 2/Largest Island.js
+++ Largest Island/Solution 2/Largest Island.js
@@ -1,56 +1,38 @@
 function largestIsland(matrix) {
-
-
-  let largestIslandCount = 0;
+  const islandSizes = [];
+  let islandId = 2;
+  
   for (let row = 0; row < matrix.length; row++) {
     for (let col = 0; col < matrix[0].length; col++) {
-      if (matrix[row][col] === 1) {
-        matrix[row][col] = 0;
-        const landCount = countPotentialLand(row, col, matrix, new Set());
-        largestIslandCount = Math.max(landCount, largestIslandCount);
-        matrix[row][col] = 1;
+      if (matrix[row][col] === 0) {
+        const countIslandAttempt = countIslandSize(row, col, matrix, islandId);
+        islandSizes.push(countIslandAttempt);
+        islandId++;
       }
     }
   }
-  return largestIslandCount;
-}
 
 
-function countPotentialLand(row, col, matrix, visited) {
-  const key = row + ',' + col;
-  if (visited.has(key)) return 0;
-  visited.add(key);
-  if (!inBounds(row, col, matrix) || matrix[row][col] === 1) return 0;
+  let maxSize = 0;
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[0].length; col++) {
+      if (matrix[row][col] !== 1) continue;
+      const neighbors = findNeighbors(row, col, matrix);
+      let islandNeighbors = new Set();
+        
+      for (let neighbor of neighbors) {
+        const [neighborRow, neighborCol] = neighbor;
+        let neighborIslandId = matrix[neighborRow][neighborCol];
+        islandNeighbors.add(neighborIslandId);
+      }
 
 
-  let count = 1;
-  const neighbors = findNeighbors(row, col, matrix);
-  for (const neighbor of neighbors) {
-    const [neighborRow, neighborCol]  = neighbor;
-    count += countPotentialLand(neighborRow, neighborCol, matrix, visited);
+      let size = 1;
+      for (const island of islandNeighbors) {
+        size += islandSizes[island - 2];
+      }
+      maxSize = Math.max(maxSize, size);
+    }
   }
-  return count; 
+  return maxSize;
 }
-
-
-function findNeighbors(row, col, matrix) {
-  const validNeighborPos = [];
-  const neighbors = [ [0, 1], [1, 0], [-1, 0], [0, -1] ];
-  for (let neighbor of neighbors){
-    const [neighborRow, neighborCol] = neighbor;
-    const newRow = neighborRow + row;
-    const newCol = neighborCol + col;
-    if (inBounds(newRow, newCol, matrix)) validNeighborPos.push([newRow, newCol]);
-  }
-  return validNeighborPos;
-}
-
-
-function inBounds(row, col, matrix) {
-  const rowInBounds = row >= 0 && row < matrix.length;
-  const colInBounds = col >= 0 && col < matrix[0].length;
-  return rowInBounds && colInBounds;
-}
-// Do not edit the line below.
-exports.largestIsland = largestIsland;
-
